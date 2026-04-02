107e4432b930c5ec0d97453acb374cc08799ed16
Jeevan Singh <s.singhjeevan1234@gmail.com>
2026-04-02 20:50:09 +0530

    Time: 4 ms (41.15%), Space: 43.2 MB (79.31%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,43 +1,43 @@
 class Solution {
-    // public boolean isValid(String s) {
-    //     Stack<Character>stack = new Stack<>();
-    //     for(char ch : s.toCharArray()){
-    //         if(ch == '(' || ch == '{' || ch == '['){
-    //             stack.push(ch);
-    //         }else{
-    //             if(stack.isEmpty()) return false;
-
-    //             char top = stack.pop();
-
-    //             if(ch == ')' && top != '(') return false;
-    //             if(ch == '}' && top != '{') return false;
-    //             if(ch == ']' && top != '[') return false;
-    //         }
-            
-    //     }
-    //     return stack.isEmpty();
-    // }
-
-
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
-
-        Map<Character, Character> map = new HashMap<>();
-        map.put(')', '(');
-        map.put('}', '{');
-        map.put(']', '[');
-
-        for(char ch : s.toCharArray()) {
-
-            if(map.containsKey(ch)) {
-                if(stack.isEmpty() || stack.pop() != map.get(ch)) {
-                    return false;
-                }
-            } else {
+        Stack<Character>stack = new Stack<>();
+        for(char ch : s.toCharArray()){
+            if(ch == '(' || ch == '{' || ch == '['){
                 stack.push(ch);
-            }
-        }
+            }else{
+                if(stack.isEmpty()) return false;
 
+                char top = stack.pop();
+
+                if(ch == ')' && top != '(') return false;
+                if(ch == '}' && top != '{') return false;
+                if(ch == ']' && top != '[') return false;
+            }
+            
+        }
         return stack.isEmpty();
     }
+
+
+    // public boolean isValid(String s) {
+    //     Stack<Character> stack = new Stack<>();
+
+    //     Map<Character, Character> map = new HashMap<>();
+    //     map.put(')', '(');
+    //     map.put('}', '{');
+    //     map.put(']', '[');
+
+    //     for(char ch : s.toCharArray()) {
+
+    //         if(map.containsKey(ch)) {
+    //             if(stack.isEmpty() || stack.pop() != map.get(ch)) {
+    //                 return false;
+    //             }
+    //         } else {
+    //             stack.push(ch);
+    //         }
+    //     }
+
+    //     return stack.isEmpty();
+    // }
 }
