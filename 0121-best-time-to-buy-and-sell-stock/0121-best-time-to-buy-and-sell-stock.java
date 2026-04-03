242382a0835239eacf126f861d534605d5a95949
Jeevan Singh <s.singhjeevan1234@gmail.com>
2026-04-03 11:51:38 +0530

    Time: 1 ms (99.92%), Space: 94.7 MB (8.83%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,33 +1,33 @@
 class Solution {
 
-        public int maxProfit(int[]prices){
-            int minPrice = prices[0];
-            int maxProfit = 0;
+        // public int maxProfit(int[]prices){
+        //     int minPrice = prices[0];
+        //     int maxProfit = 0;
 
-            for(int sell = 1; sell < prices.length; sell++){
-                if(minPrice > prices[sell]){
-                    minPrice = prices[sell];
-                }
-                maxProfit = Math.max(maxProfit, prices[sell] - minPrice);
+        //     for(int sell = 1; sell < prices.length; sell++){
+        //         if(minPrice > prices[sell]){
+        //             minPrice = prices[sell];
+        //         }
+        //         maxProfit = Math.max(maxProfit, prices[sell] - minPrice);
+        //     }
+
+        //     return maxProfit;
+        // }
+
+    public int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for(int price : prices){
+            if(price < minPrice){
+                minPrice = price;
             }
 
-            return maxProfit;
+            if(price - minPrice > maxProfit){
+                maxProfit = price - minPrice;
+            }
         }
 
-    // public int maxProfit(int[] prices) {
-    //     int minPrice = Integer.MAX_VALUE;
-    //     int maxProfit = 0;
-
-    //     for(int price : prices){
-    //         if(price < minPrice){
-    //             minPrice = price;
-    //         }
-
-    //         if(price - minPrice > maxProfit){
-    //             maxProfit = price - minPrice;
-    //         }
-    //     }
-
-    //     return maxProfit;
-    // }
+        return maxProfit;
+    }
 }
