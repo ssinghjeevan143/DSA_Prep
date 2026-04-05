44244065f3900b45c953ca04cf173580dd8583b2
Jeevan Singh <s.singhjeevan1234@gmail.com>
2026-04-05 22:56:43 +0530

    Time: 2 ms (91.01%), Space: 48 MB (12.58%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,16 +1,41 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-
-        int start = 0;
-        
-        for(int num : nums){
-            if(num  != 0){
-                nums[start++] = num;
+          int i = 0; // slow pointer
+            for (int j = 0; j < nums.length; j++) { // fast pointer
+                if (nums[j] != 0) {
+                    nums[i] = nums[j];
+                    i++;
+                }
             }
-        }
+            while (i < nums.length) {
+                nums[i] = 0;
+                i++;
+            }
 
-        while(start < nums.length){
-            nums[start++] = 0;
-        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // int start = 0;
+        
+        // for(int num : nums){
+        //     if(num  != 0){
+        //         nums[start++] = num;
+        //     }
+        // }
+
+        // while(start < nums.length){
+        //     nums[start++] = 0;
+        // }
     }
 }
