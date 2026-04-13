076fd047447f9579a125234d961b8e048c7b3a02
Jeevan Singh <s.singhjeevan1234@gmail.com>
2026-04-13 21:59:20 +0530

    Time: 2 ms (97.74%), Space: 45 MB (22.83%) - LeetHub

---
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+        HashSet<Integer> seen = new HashSet<>();
+        for (int num : arr) {
+            if (seen.contains(2 * num) || (num % 2 == 0 && seen.contains(num / 2))) {
+                return true;
+            }
+            seen.add(num);
+        }
+        return false;
+    }
+}
