c584c609494c27e7be49233bbb754352326e3972
Jeevan Singh <s.singhjeevan1234@gmail.com>
2026-04-14 11:24:14 +0530

    Time: 3 ms (70.21%), Space: 49 MB (70.21%) - LeetHub

---
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] replaceElements(int[] arr) {
+        int maxRight = -1;
+        for(int i = arr.length-1;i>=0;i--){
+            int current = arr[i];
+            arr[i] = maxRight;
+            maxRight = Math.max(maxRight, current);
+        }
+        return arr;
+    }
+}
