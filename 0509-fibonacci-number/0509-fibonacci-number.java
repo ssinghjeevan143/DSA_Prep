f425f8da6daad0effda971878df68df9b45c61e8
Jeevan Singh <s.singhjeevan1234@gmail.com>
2026-04-26 21:00:47 +0530

    Time: 0 ms (100%), Space: 42.3 MB (17.22%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,7 +1,22 @@
 class Solution {
-    public int fib(int n) {
-        if(n==0) return 0;
-        if(n==1) return 1;
-        return fib(n-1) + fib(n-2);
+    public int fib(int n){
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+        int n1 = 0;
+        int n2 = 1;
+        int n3 = 0;
+        for(int i = 2;i <= n; i++){
+            n3 = n1 + n2;
+            n1 = n2;
+            n2 = n3;
+        }
+        return n3;
     }
+
+
+    // public int fib(int n) {
+    //     if(n==0) return 0;
+    //     if(n==1) return 1;
+    //     return fib(n-1) + fib(n-2);
+    // }
 }
