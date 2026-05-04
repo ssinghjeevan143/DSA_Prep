86fdfe9dd57d016293188e419011bfed4e964c56
Jeevan Singh <s.singhjeevan1234@gmail.com>
2026-05-04 21:53:18 +0530

    Time: 1 ms (68.51%), Space: 42.1 MB (73.51%) - LeetHub

--- 1952-three-divisors/1952-three-divisors.java
+++ 1952-three-divisors/1952-three-divisors.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean isThree(int n) {
         int count = 0;
 
-        // हर number को check करो कि divisor है या नहीं
+      
         for (int i = 1; i <= n; i++) {
             if (n % i == 0) {
                 count++;
