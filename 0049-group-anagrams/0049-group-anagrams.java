f6276badcd15dfe1729ce645bd8530dc78add272
Jeevan Singh <s.singhjeevan1234@gmail.com>
2026-05-14 22:39:53 +0530

    Time: 6 ms (99.49%), Space: 49.4 MB (91.6%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public static List<List<String>> groupAnagrams(String[] strs) {
+      
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        for (String s : strs) {
+            
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+
+         
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+            map.get(key).add(s);
+        }
+
+      
+        return new ArrayList<>(map.values());
+    }
+
+}
