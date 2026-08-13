6588512dde61519e7d95659cc949039326806c83
Jeevan Singh <s.singhjeevan1234@gmail.com>
2026-08-13 23:24:25 +0530

    Time: 3 ms (88.17%), Space: 48.1 MB (84.58%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -1,42 +1,42 @@
 class Solution {
-    public boolean searchMatrix(int[][]matrix,int target){
-        int row = matrix.length;
-        int col = matrix[0].length;
+    // public boolean searchMatrix(int[][]matrix,int target){
+    //     int row = matrix.length;
+    //     int col = matrix[0].length;
 
-        for(int i = 0; i < row;i++){
-            for(int j = 0; j < col;j++){
-                if(matrix[i][j] == target){
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-
-
-
-
-    // public boolean searchMatrix(int[][] matrix, int target) {
-    //     int nor = matrix.length;
-    //     int noc = matrix[0].length;
-
-    //     int row = 0;
-    //     int col = noc - 1;
-
-    //     while(row < nor && col >= 0){
-    //         int val = matrix[row][col];
-
-    //         if(val == target){
-    //             return true;
-    //         }else if(val > target){
-    //             col--;
-    //         }else{
-    //             row++;
+    //     for(int i = 0; i < row;i++){
+    //         for(int j = 0; j < col;j++){
+    //             if(matrix[i][j] == target){
+    //                 return true;
+    //             }
     //         }
     //     }
-
     //     return false;
-
     // }
+
+
+
+
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int nor = matrix.length;
+        int noc = matrix[0].length;
+
+        int row = 0;
+        int col = noc - 1;
+
+        while(row < nor && col >= 0){
+            int val = matrix[row][col];
+
+            if(val == target){
+                return true;
+            }else if(val > target){
+                col--;
+            }else{
+                row++;
+            }
+        }
+
+        return false;
+
+    }
 }
