aa5fba6c67fc2428fae1e79949421e07d1acfdff
Jeevan Singh <s.singhjeevan1234@gmail.com>
2026-08-25 20:19:48 +0530

    Time: 1 ms (29.7%), Space: 47.5 MB (33.04%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -2,6 +2,26 @@ import java.util.HashSet;
 
 class Solution {
     public int missingNumber(int[] nums) {
+//cyclic sort
+        int i = 0;
+        
+        while(i < nums.length){
+            int correct = nums[i];
+            if(nums[i] < nums.length && nums[i] != nums[correct]){
+                int temp = nums[i];
+            nums[i] = nums[correct];
+            nums[correct] = temp;
+            }else{
+                i++;
+            }
+            
+        }
+        for(int index = 0 ; index < nums.length; index++){
+            if(nums[index] != index ){
+                return index;
+            }
+        }
+        return nums.length;
 
         // HashSet<Integer> set = new HashSet<>();
 
@@ -28,13 +48,13 @@ class Solution {
 
 
 
-        int n = nums.length;
-        int expectedSum = n * (n + 1)/2;
+        // int n = nums.length;
+        // int expectedSum = n * (n + 1)/2;
 
-        int actualSum = 0;
-        for(int num : nums){
-            actualSum += num;
-        }
-        return expectedSum - actualSum;
+        // int actualSum = 0;
+        // for(int num : nums){
+        //     actualSum += num;
+        // }
+        // return expectedSum - actualSum;
     }
 }
