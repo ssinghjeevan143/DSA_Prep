41e6fcdef916d1aa73b138dffb729bcdfa86275f
Jeevan Singh <s.singhjeevan1234@gmail.com>
2026-09-05 17:37:29 +0530

    Time: 57 ms (33.64%), Space: 99.6 MB (13.9%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -1,29 +1,62 @@
 class Solution {
+    //[1,5,4,2,9,9,9]
 
     public long maximumSubarraySum(int[]nums, int k){
-        long max = 0, sum = 0;
-        Set<Integer>set = new HashSet<>();
+        long max= 0,sum = 0;
+        Map<Integer, Integer> freq = new HashMap<>();
         int n = nums.length;
-        int left = 0;
-        for(int right = 0; right < n; right++){
-            while(set.contains(nums[right])){
-                set.remove(nums[left]);
-                sum -= nums[left];
-                left++;
+        for(int i = 0; i < n; i++){
+            sum += nums[i];
+            freq.put(nums[i], freq.getOrDefault(nums[i],0)+1);
+
+            if(i >= k){
+                sum -= nums[i - k];
+                freq.put(nums[i-k],freq.get(nums[i-k]) - 1);
+                if(freq.get(nums[i-k])== 0){
+                    freq.remove(nums[i-k]);
+                }
             }
 
-            set.add(nums[right]);
-            sum += nums[right];
-
-            if(right - left + 1 == k){
+            if( i >= k - 1 && freq.size() == k){
                 max = Math.max(max,sum);
-                set.remove(nums[left]);
-                sum -= nums[left];
-                left++;
             }
+            
         }
         return max;
     }
+
+
+
+
+
+
+
+
+
+    // public long maximumSubarraySum(int[]nums, int k){
+    //     long max = 0, sum = 0;
+    //     Set<Integer>set = new HashSet<>();
+    //     int n = nums.length;
+    //     int left = 0;
+    //     for(int right = 0; right < n; right++){
+    //         while(set.contains(nums[right])){
+    //             set.remove(nums[left]);
+    //             sum -= nums[left];
+    //             left++;
+    //         }
+
+    //         set.add(nums[right]);
+    //         sum += nums[right];
+
+    //         if(right - left + 1 == k){
+    //             max = Math.max(max,sum);
+    //             set.remove(nums[left]);
+    //             sum -= nums[left];
+    //             left++;
+    //         }
+    //     }
+    //     return max;
+    // }
 
 
     // public long maximumSubarraySum(int[] nums, int k) {
