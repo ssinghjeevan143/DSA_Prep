4a52b5db7f367326a9bbd3ddf5b80f2fbad16d86
Jeevan Singh <s.singhjeevan1234@gmail.com>
2026-09-13 22:46:05 +0530

    Time: 1 ms (99.84%), Space: 69.2 MB (47.28%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -1,18 +1,57 @@
 class Solution {
+//sliding window pattern
+public int minSubArrayLen(int target, int[]nums){
+    int size = Integer.MAX_VALUE;
+    int sum = 0;
+    int i = 0;
+    int j = 0;
+    while(j < nums.length){
+        sum += nums[j];
 
-    public int minSubArrayLen(int target, int[] nums) {
-        int sum = 0;
-        int minLength = Integer.MAX_VALUE;
-        int i = 0;
-        for(int j = 0; j < nums.length; j++){
-            sum += nums[j];
-
-            while(sum >= target){
-                minLength = Math.min(minLength, j-i +1);
-                sum -= nums[i];
-                i++;
-            }
+        while(sum >= target){
+            size = Math.min(size, j - i+1);
+            sum -= nums[i];
+            i++;
         }
-        return minLength == Integer.MAX_VALUE ? 0 : minLength;
+        j++;
     }
+    return size == Integer.MAX_VALUE ? 0 : size;
+}
+
+
+
+
+// // brute force  TC: O(n^2)
+//     public int minSubArrayLen(int target, int[]nums){
+//         int size = Integer.MAX_VALUE;
+//         int sum = 0;
+//         for(int i = 0; i < nums.length;i++){
+//              sum = 0;
+//             for(int j = i; j < nums.length; j++){
+//                 sum += nums[j];
+//                 if(sum >= target){
+//                     size = Math.min(size, j - i + 1);
+//                     break;
+//                 }
+                
+//             }
+//         }
+//         return size == Integer.MAX_VALUE ? 0 : size;
+//     }
+
+    // public int minSubArrayLen(int target, int[] nums) {
+    //     int sum = 0;
+    //     int minLength = Integer.MAX_VALUE;
+    //     int i = 0;
+    //     for(int j = 0; j < nums.length; j++){
+    //         sum += nums[j];
+
+    //         while(sum >= target){
+    //             minLength = Math.min(minLength, j-i +1);
+    //             sum -= nums[i];
+    //             i++;
+    //         }
+    //     }
+    //     return minLength == Integer.MAX_VALUE ? 0 : minLength;
+    // }
 }
