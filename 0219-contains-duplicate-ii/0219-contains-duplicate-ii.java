9cd26bcb509c398d253f1cc81a2e4fa54dd8fec2
Jeevan Singh <s.singhjeevan1234@gmail.com>
2026-09-14 23:19:59 +0530

    Time: 31 ms (41.05%), Space: 104.1 MB (77.37%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,6 +1,18 @@
 import java.util.HashSet;
 
 class Solution {
+    //BRUTE FORCE APPROACH
+    // public boolean containsNearbyDuplicate(int[] nums, int k) {
+    //     for(int i = 0; i < nums.length; i++){
+    //         for(int j = i+1; j < nums.length;j++){
+    //             if(nums[i] == nums[j] && Math.abs(i - j) <= k){
+    //                 return true;
+    //             }
+    //         }
+    //     }
+    //     return false;
+    // }
+
 
     public boolean containsNearbyDuplicate(int[] nums, int k) {
 
