3b46c1ca39344627a0b850e43d7192d7256ad30c
Jeevan Singh <s.singhjeevan1234@gmail.com>
2026-09-15 23:34:45 +0530

    Time: 4 ms (42.31%), Space: 69.7 MB (25.79%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -1,32 +1,34 @@
 class Solution {
-    // public double findMaxAverage(int[] nums, int k) {
-    //     int sum = 0;
-
-    //     for(int i=0; i < k;i++){
-    //         sum += nums[i];
-    //     }
-    //     int maxSum = sum;
-
-    //     for(int i = k; i < nums.length; i++){
-    //         sum = sum - nums[i - k] + nums[i];
-    //         maxSum = Math.max(maxSum,sum);
-    //     }
-    //     return (double) maxSum/k;
- 
-    // }
-
-
-
     public double findMaxAverage(int[] nums, int k) {
-        double maxAvg = Double.NEGATIVE_INFINITY; 
-        for(int i =0; i <= nums.length - k; i++){
-            int sum = 0;
-            for(int j = i; j < i+ k; j++){
-                sum += nums[j];
-            }
-            double avg = (double)sum/k;
-            maxAvg = Math.max(avg,maxAvg);
+       int sum = 0;
+       double max = 0;
+       for(int i = 0; i < k; i++){
+        sum += nums[i];
+       }
+       max = sum;
+    
+        for(int i = k ; i < nums.length;i++){
+            sum = sum + nums[i] - nums[i - k];
+            max = Math.max(sum, max);
         }
-        return maxAvg;
+        
+        
+        return max/k;
     }
+
+
+
+// BRUTE FORCE APPROACH
+    // public double findMaxAverage(int[] nums, int k) {
+    //     double maxAvg = Double.NEGATIVE_INFINITY; 
+    //     for(int i =0; i <= nums.length - k; i++){
+    //         int sum = 0;
+    //         for(int j = i; j < i+ k; j++){
+    //             sum += nums[j];
+    //         }
+    //         double avg = (double)sum/k;
+    //         maxAvg = Math.max(avg,maxAvg);
+    //     }
+    //     return maxAvg;
+    // }
 }
