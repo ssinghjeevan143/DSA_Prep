170387d87aeb5e39b8253f092c46a63e0c49b0fe
Jeevan Singh <s.singhjeevan1234@gmail.com>
2026-09-16 22:24:07 +0530

    Time: 1086 ms (5.01%), Space: 48.3 MB (5.54%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,26 +1,19 @@
 class Solution {
-    public int lengthOfLongestSubstring(String s) {
-        int[]lastIndex = new int[256];
-        Arrays.fill(lastIndex, -1);
-        int left = 0;
-        int maxLen = 0;
-        for(int right = 0; right < s.length();right++){
-            char c = s.charAt(right);
-            if(lastIndex[c] >= left){
-                left = lastIndex[c] + 1;
-            }
-            lastIndex[c] = right;
-            maxLen = Math.max(maxLen, right-left + 1);
-        }
-        return maxLen;
-    }
-
-
-
-
-
-
-
+    // public int lengthOfLongestSubstring(String s) {
+    //     int[]lastIndex = new int[256];
+    //     Arrays.fill(lastIndex, -1);
+    //     int left = 0;
+    //     int maxLen = 0;
+    //     for(int right = 0; right < s.length();right++){
+    //         char c = s.charAt(right);
+    //         if(lastIndex[c] >= left){
+    //             left = lastIndex[c] + 1;
+    //         }
+    //         lastIndex[c] = right;
+    //         maxLen = Math.max(maxLen, right-left + 1);
+    //     }
+    //     return maxLen;
+    // }
 
 
     // public int lengthOfLongestSubstring(String s) {
@@ -37,6 +30,24 @@ class Solution {
     //    }
     //    return maxLen;
     // }
+
+
+    public int lengthOfLongestSubstring(String s){
+        int max = 0;
+        for(int i = 0; i < s.length();i++){
+            
+            Set<Character>set = new HashSet<>();
+            for(int j = i; j < s.length();j++){
+                char c = s.charAt(j);
+                if(set.contains(s.charAt(j))) break;
+
+                set.add(c);
+                max = Math.max(max, j-i+1);
+            }
+            
+        }
+        return max;
+    }
 }
 
 
