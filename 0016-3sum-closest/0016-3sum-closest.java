1e611de57e822128f8cb53d455feb7bbee8e5c77
Jeevan Singh <s.singhjeevan1234@gmail.com>
2026-09-23 21:48:00 +0530

    Time: 18 ms (38.85%), Space: 45.9 MB (15.05%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -1,22 +1,46 @@
 class Solution {
-    public int threeSumClosest(int[] nums, int target) {
+    // public int threeSumClosest(int[] nums, int target) {
+    //     Arrays.sort(nums);
+    //     int closestSum = nums[0] + nums[1] + nums[2];
+    //     for(int i = 0; i < nums.length - 2; i++ ){
+    //         int left = i + 1;
+    //         int right = nums.length - 1;
+    //         while(left  < right){
+    //             int sum = nums[i] + nums[left] + nums[right];
+    //             if(Math.abs(sum - target) < Math.abs(closestSum - target)){
+    //                 closestSum = sum;
+    //             }
+
+    //             if(sum < target){
+    //                 left++;
+    //             }else{
+    //                 right--;
+    //             }
+                
+    //         }
+    //     }
+    //     return closestSum;
+    // }
+
+     public int threeSumClosest(int[] nums, int target) {
         Arrays.sort(nums);
         int closestSum = nums[0] + nums[1] + nums[2];
-        for(int i = 0; i < nums.length - 2; i++ ){
-            int left = i + 1;
-            int right = nums.length - 1;
-            while(left  < right){
-                int sum = nums[i] + nums[left] + nums[right];
-                if(Math.abs(sum - target) < Math.abs(closestSum - target)){
-                    closestSum = sum;
-                }
 
-                if(sum < target){
+        for(int i = 0; i < nums.length - 2; i++){
+            int left = i+1;
+            int right = nums.length-1;
+
+            while(left < right){
+                int currentSum = nums[i]+nums[left] + nums[right];
+
+                if(Math.abs(currentSum - target) < Math.abs(closestSum - target)){
+                    closestSum = currentSum;
+                }
+                if(currentSum < target){
                     left++;
                 }else{
                     right--;
                 }
-                
             }
         }
         return closestSum;
