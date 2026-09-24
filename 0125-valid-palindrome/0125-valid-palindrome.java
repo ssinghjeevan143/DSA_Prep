f391e3a48f91e5ecab57291d60adb8b74fcb9ba6
Jeevan Singh <s.singhjeevan1234@gmail.com>
2026-09-24 23:02:32 +0530

    Time: 253 ms (5.1%), Space: 48.3 MB (5.05%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -25,26 +25,48 @@ class Solution {
     // }
 
 
+    // public boolean isPalindrome(String s){
+
+    //     int l = 0;
+    //     int e = s.length() - 1;
+
+    //     while(l < e){
+    //         while(l < e && !Character.isLetterOrDigit(s.charAt(l))){
+    //             l++;
+    //         }
+    //         while(l < e && !Character.isLetterOrDigit(s.charAt(e))){
+    //             e--;
+    //         }
+
+    //         if(Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(e))){
+    //             return false;
+    //         }
+
+    //         l++;
+    //         e--;
+    //     }
+    //     return true;
+    // }
+
     public boolean isPalindrome(String s){
-
-        int l = 0;
-        int e = s.length() - 1;
-
-        while(l < e){
-            while(l < e && !Character.isLetterOrDigit(s.charAt(l))){
-                l++;
+        String str = s.toLowerCase();
+        String rev = "";
+        for(int i = str.length()-1; i >= 0; i--){
+            if(Character.isLetterOrDigit(str.charAt(i))){
+                rev += str.charAt(i);
             }
-            while(l < e && !Character.isLetterOrDigit(s.charAt(e))){
-                e--;
-            }
-
-            if(Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(e))){
-                return false;
-            }
-
-            l++;
-            e--;
         }
-        return true;
+
+        String forward = "";
+        for(int i = 0; i < str.length();i++){
+            if(Character.isLetterOrDigit(str.charAt(i))){
+                forward += str.charAt(i);
+            }
+        }
+
+        if(forward.equals(rev)){
+            return true;
+        }
+        return false;
     }
 }
