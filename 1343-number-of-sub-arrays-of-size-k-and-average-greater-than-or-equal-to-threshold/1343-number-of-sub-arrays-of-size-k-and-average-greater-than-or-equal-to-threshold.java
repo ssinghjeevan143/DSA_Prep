0d2268da9260487bc9635edaa0d2e1d7a7bc8471
Jeevan Singh <s.singhjeevan1234@gmail.com>
2026-09-24 22:53:13 +0530

    Time: 3 ms (88.09%), Space: 71.7 MB (62.66%) - LeetHub

--- 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.java
+++ 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.java
@@ -1,17 +1,40 @@
 class Solution {
-    public int numOfSubarrays(int[] arr, int k, int threshold) {
+    // public int numOfSubarrays(int[] arr, int k, int threshold) {
         
-        int count = 0;
+    //     int count = 0;
 
-        for(int i = 0; i <= arr.length - k; i++ ){
-            int sum = 0;
-            for(int j = i; j < i+k;j++){
-                sum += arr[j];
+    //     for(int i = 0; i <= arr.length - k; i++ ){
+    //         int sum = 0;
+    //         for(int j = i; j < i+k;j++){
+    //             sum += arr[j];
                 
+    //         }
+    //         if(sum >= k*threshold){
+    //                 count++;
+    //             }
+    //     }
+    //     return count;
+    // }
+
+//SLIDING WINDOW APPROACH
+    public int numOfSubarrays(int[] arr, int k, int threshold) {
+       int count = 0;
+       int maxT = k * threshold;
+       int sum = 0;
+        for(int i = 0; i < k; i++){
+            sum+=arr[i];
+        }
+
+        if(sum >= maxT){
+            count++;
+        }
+        for(int i = k; i < arr.length; i++){
+            sum = sum + arr[i];
+            sum = sum - arr[i - k];
+
+            if(sum >= maxT){
+                count++;
             }
-            if(sum >= k*threshold){
-                    count++;
-                }
         }
         return count;
     }
